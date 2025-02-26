2fc55800c363ce6698c968f78d62bf083feca332
oxyum services <29GariglioSe@fortcherry.org>
2025-02-26 01:30:37 -0500

    push

--- script.js
+++
@@ -1,4 +0,0 @@
-fetch('http://shortline.proxy.rlwy.net:16258/cloudbrowser')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
